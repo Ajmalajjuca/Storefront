eb5898c8d06be8d931bed21a8c544d7038392e0a
ajmal <ajmalajju313786@gmail.com>
2026-08-10 21:55:06 +0530

    refactor: update font family in layout and scroll stage for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,7 +96,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&family=Open+Sans:wght@300;400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Open+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
